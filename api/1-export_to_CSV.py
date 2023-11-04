068a523b63fb10eae20c6e1fa2059f7b2009121f
Steve SHYAKA <s.shyaka1@alustudent.com>
2023-11-04 11:19:40 -0700

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """
-Module to retrieve and display employee TODO list progress and export in CSV format.
+Module to retrieve and display 
+employee TODO list progress and export in CSV format.
 """
 
 import requests
 import csv
 from sys import argv
 
+
 def get_employee_todo_progress(employee_id):
     """
-    Function to get and display employee TODO list progress and export in CSV format.
+    Function to get and display employee TODO list progress and 
+    export in CSV format.
     """
     # API endpoint for employee information
     user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     # API endpoint for employee's TODO list
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    todo_url = 
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
 
     # Fetching user information
     user_response = requests.get(user_url)
@@ -30,14 +34,18 @@ def get_employee_todo_progress(employee_id):
     csv_file_path = f"{user_id}.csv"
     with open(csv_file_path, mode='w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
+            "TASK_TITLE"])
 
         for task in todo_data:
-            task_completed_status = "COMPLETED" if task.get("completed") else "NOT COMPLETED"
+            task_completed_status =
+            "COMPLETED" if task.get("completed") else "NOT COMPLETED"
             task_title = task.get("title")
-            csv_writer.writerow([user_id, username, task_completed_status, task_title])
+            csv_writer.writerow
+            ([user_id, username, task_completed_status, task_title])
 
-    print(f"Employee {username} is done with tasks ({user_id}/{len(todo_data)}):")
+    print(f"Employee {username}
+            is done with tasks ({user_id}/{len(todo_data)}):")
     for task in todo_data:
         print(f"\t{task['title']}")
 
