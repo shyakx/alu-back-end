11583efff6faa95a31eaa8738125b6dc68f7b38e
Steven SHYAKA <133809942+shyakx@users.noreply.github.com>
2023-11-05 09:02:40 +0200

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
 """
-Module to retrieve and display employee TODO list progress.
+Gathers data from an API based on employee ID.
 """
 
 import requests
-from sys import argv
+import sys
 
-def get_employee_todo_progress(employee_id):
-    """
-    Function to get and display employee TODO list progress.
-    """
-    # API endpoint for employee information
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    # API endpoint for employee's TODO list
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
-    # Fetching user information
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
-
-    # Fetching TODO list
-    todo_response = requests.get(todo_url)
-    todo_data = todo_response.json()
-
-    # Filtering completed tasks
-    completed_tasks = [task for task in todo_data if task.get("completed")]
-
-    # Displaying progress
-    print(f"Employee {employee_name} is done with tasks ({len(completed_tasks)}/{len(todo_data)}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
+""" Function to gather data from an API """
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: python script.py <employee_id>")
-    else:
-        employee_id = int(argv[1])
-        get_employee_todo_progress(employee_id)
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
 
+    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    todo = todo.format(employee_id)
+
+    user_info = requests.request("GET", url).json()
+    todo_info = requests.request("GET", todo).json()
+
+    employee_name = user_info.get("name")
+    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
+    task_com = len(total_tasks)
+    total_task_done = len(todo_info)
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          task_com, total_task_done))
+
+    [print("\t {}".format(task.get("title"))) for task in total_tasks]
