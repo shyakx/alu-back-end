938cfbf272b7f5c9432b3c6f5e178c261c9aeb97
Steven SHYAKA <133809942+shyakx@users.noreply.github.com>
2023-11-05 09:54:30 +0200

    3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,55 +1,24 @@
 #!/usr/bin/python3
+"""
+Exports tasks of all employees to a JSON file.
+"""
 
 import json
-
-def export_tasks_to_json(tasks):
-    """
-    Export tasks to a JSON file for all employees.
-
-    :param tasks: Dictionary containing tasks for all employees
-    :type tasks: dict
-    """
-    # Write the dictionary to a JSON file
-    filename = "todo_all_employees.json"
-    with open(filename, "w") as json_file:
-        json.dump(tasks, json_file, indent=2)
-
-def organize_tasks_by_employee(tasks):
-    """
-    Organize tasks by employee.
-
-    :param tasks: List of tasks
-    :type tasks: list
-    :return: Dictionary containing tasks organized by employee
-    :rtype: dict
-    """
-    organized_tasks = {}
-
-    for task in tasks:
-        user_id = task.get("user_id", "")
-        username = task.get("username", "")
-
-        if user_id not in organized_tasks:
-            organized_tasks[user_id] = []
-
-        task_info = {
-            "username": username,
-            "task": task.get("title", ""),
-            "completed": task.get("completed", False)
-        }
-
-        organized_tasks[user_id].append(task_info)
-
-    return organized_tasks
+import requests
 
 if __name__ == "__main__":
-    # Example usage with tasks for multiple users
-    tasks = [
-        {"user_id": "123", "title": "Task 1", "completed": True, "username": "john_doe"},
-        {"user_id": "123", "title": "Task 2", "completed": False, "username": "john_doe"},
-        {"user_id": "456", "title": "Task 3", "completed": True, "username": "jane_smith"},
-        # Add more tasks as needed
-    ]
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+    """
+        export to JSON
+    """
 
-    organized_tasks = organize_tasks_by_employee(tasks)
-    export_tasks_to_json(organized_tasks)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
